238a5e37d3a46a785cecef00a14eceb504766b8d
christinelai00 <christine.lai.00@gmail.com>
2022-11-20 15:00:24 -0500

    adding refresh token

--- pages/api/strava/athlete/[id].ts
+++ pages/api/strava/athlete/[id].ts
@@ -10,22 +10,8 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   const { id } = req.query;
-  console.log(id);
   const url = `https://www.strava.com/api/v3/athletes/${id}/stats`;
 
-  // const options = {
-  //   method: "GET",
-  //   headers: {
-  //     "Authorization": "2a036e7349835df41968e49329e14c839007cc7e"
-  //   }
-  // };
-
-  // await fetch(url, options)
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log(data);
-  //     res.json(data);
-  //   });
 
   await axios
     .get(url, { headers: { 'Authorization': `Bearer ${process.env.ACCESS_TOKEN!}` } })
@@ -38,20 +24,3 @@ export default async function handler(
       res.status(400).json({ err });
     });
 }
-
-// export async function getStaticProps() {
-//   const options = {
-//     method: "GET",
-//     headers: new Headers({'content-type': 'application/json'}),
-//     mode: 'no-cors'
-// };
-//   const res = await
-//   fetch("https://www.strava.com/api/v3/athlete");
-//   const allPostsData = await res.json();
-//   return {
-//       props: {
-//           allPostsData,
-//       },
-//       revalidate: 30,
-//   };
-// }
--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Image from 'next/image';
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,23 +9,11 @@ const Wrapper = styled.div`
 
 function Stats() {
   const [name, setName] = useState('');
-  const [id, setId] = useState(0);
+  const [id, setId] = useState(35558561);
+  const [city, setCity] = useState('');
   const [yearlyDistance, setYearlyDistance] = useState(0);
-  const getAthlete = () => {
-    const postData = async () => {
-      const response = await fetch("/api/strava/athlete", {
-        method: "GET",
-      });
-      return response.json();
-    };
 
-    postData().then((data) => {
-      console.log(data);
-      setId(data.id);
-      setName(`${data.firstname} ${data.lastname}`);
-      // alert(data.name);
-    });
-  };
+
 
   const getAthleteStats = () => {
     const postData = async () => {
@@ -43,17 +30,34 @@ function Stats() {
   };
 
 
-
   return (
     <Wrapper>
       <h2>Strava Stats</h2>
       <p>This page will have stats</p>
-      <button onClick={getAthlete}>Click me ;)</button>
-      <div>{name}</div>
+
+      {/* <div>{athlete.firstname}</div>
+      <div>{athlete.city}</div> */}
       <button onClick={getAthleteStats}>get stats</button>
       <div>{yearlyDistance / 100} kms</div>
+      {/* <button onClick={getActivity}>get stats</button> */}
     </Wrapper>
   );
 }
+
+// export async function getStaticProps() {
+
+//   const server = 'http://localhost:3000';
+
+//   const res = await fetch(`${server}/api/strava/athlete`);
+//   const stats = await res.json();
+
+//   process.env.ACCESS_TOKEN = 'acdf263dfd8e2aea6dfc8e466554f7a0aa7616f4';
+
+//   return {
+//     props: {
+//       stats
+//     }
+//   }
+// }
 
 export default Stats;
